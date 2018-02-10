6539a6b506a751158855e8482052aa13d1eae699
Daniil Chernyshev <des1nteresado@outlook.com>
2018-02-10 19:33:13 +0300

    first com

---
+++ 1st lab/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "И";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ИЛИ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(252, 141);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "ИСКЛ. ИЛИ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(333, 141);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "НЕ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(73, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(276, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(73, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(211, 181);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(73, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(211, 299);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "8";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(211, 328);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(211, 270);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "10";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(211, 241);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "16";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(155, 104);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 21);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "2";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(198, 104);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(37, 21);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "8";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(241, 104);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(45, 21);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "10";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(292, 104);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(45, 21);
+            this.radioButton4.TabIndex = 14;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "16";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 376);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ 1st lab/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int perem1;
+            //Int32.TryParse(textBox1.Text, out perem1);
+            //int perem2;
+            //Int32.TryParse(textBox2.Text, out perem2);
+            //int pr = perem1 | perem2;
+            //string s = "";
+            //s += pr;
+            //textBox3.Text = s;
+            if(radioButton1.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 & pr2;
+                textBox3.Text = Convert.ToString(or, 2);
+            }
+            if (radioButton2.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 & pr2;
+                textBox3.Text = Convert.ToString(or, 8);
+            }
+            if (radioButton3.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 & pr2;
+                textBox3.Text = Convert.ToString(or, 10);
+            }
+            if (radioButton4.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 & pr2;
+                textBox3.Text = Convert.ToString(or, 16);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox3.Text);
+            textBox3.Text = Convert.ToString(i, 8);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox3.Text);
+            textBox3.Text = Convert.ToString(i, 10);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox3.Text);
+            textBox3.Text = Convert.ToString(i, 2);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox3.Text);
+            textBox3.Text = Convert.ToString(i, 16);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 | pr2;
+                textBox3.Text = Convert.ToString(or, 2);
+            }
+            if (radioButton2.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 | pr2;
+                textBox3.Text = Convert.ToString(or, 8);
+            }
+            if (radioButton3.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 | pr2;
+                textBox3.Text = Convert.ToString(or, 10);
+            }
+            if (radioButton4.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 | pr2;
+                textBox3.Text = Convert.ToString(or, 16);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 ^ pr2;
+                textBox3.Text = Convert.ToString(or, 2);
+            }
+            if (radioButton2.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 ^ pr2;
+                textBox3.Text = Convert.ToString(or, 8);
+            }
+            if (radioButton3.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 ^ pr2;
+                textBox3.Text = Convert.ToString(or, 10);
+            }
+            if (radioButton4.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = pr1 ^ pr2;
+                textBox3.Text = Convert.ToString(or, 16);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+               
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = ~(pr1 + pr2);
+                textBox3.Text = Convert.ToString(or, 2);
+            }
+            if (radioButton2.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = ~(pr1 + pr2);
+                textBox3.Text = Convert.ToString(or, 8);
+            }
+            if (radioButton3.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = ~(pr1 + pr2);
+                textBox3.Text = Convert.ToString(or, 10);
+            }
+            if (radioButton4.Checked)
+            {
+                int pr1 = Convert.ToInt32(textBox1.Text);
+                int pr2 = Convert.ToInt32(textBox2.Text);
+                int or = ~(pr1 + pr2);
+                textBox3.Text = Convert.ToString(or, 16);
+            }
+        }
+    }
+}
