e7859bc16137a6e648bbf807a99453ee7ed53657
Daniil Chernyshev <des1nteresado@outlook.com>
2018-04-21 19:26:35 +0300

    create 9s lab, but it have a lot bugs:D

---
+++ 9s lab/WpfApp1/WpfApp1/ChannelContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WpfApp1
+{
+    class ChannelContext : DbContext
+    {
+        public ChannelContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Channel> Channels { get; set; }
+        public DbSet<ChannelImage> ChannelImages { get; set; }
+    }
+}
---
+++ 9s lab/WpfApp1/WpfApp1/EFGenericRep.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class EFGenericRep<TEntity> : InterfaceGenericRep<TEntity> where TEntity : class
+    {
+        DbContext _context;
+        DbSet<TEntity> _dbSet;
+
+        public EFGenericRep(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> Get()
+        {
+            return _dbSet.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<TEntity> Get(Func<TEntity, bool> predicate)
+        {
+            return _dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+        public TEntity FindById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Create(TEntity item)
+        {
+            _dbSet.Add(item);
+            _context.SaveChanges();
+        }
+        public void Update(TEntity item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        public void Remove(TEntity item)
+        {
+            _context.Entry(item).State = EntityState.Deleted;
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
+    }
+}
+//не удаляется, плохо работает связь!
